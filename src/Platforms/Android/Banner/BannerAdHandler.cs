3dfef5f99843b7f7aa8ae5db13a0a618e0ca1c9a
Jake Soenneker <jake@soenneker.com>
2025-02-28 08:26:25 -0600

    No longer constructor initializing event unsubscribing Loading ad timing changes

--- src/Platforms/Android/Banner/BannerAdHandler.cs
+++ src/Platforms/Android/Banner/BannerAdHandler.cs
@@ -1,6 +1,6 @@
-﻿using System.Collections.Generic;
-using Android.Gms.Ads;
+﻿using Android.Gms.Ads;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Maui;
 using Microsoft.Maui.Handlers;
 using Soenneker.Dtos.ProblemDetails;
@@ -9,6 +9,8 @@ using Soenneker.Maui.Admob.Constants;
 using Soenneker.Maui.Admob.Enums;
 using Soenneker.Maui.Admob.Platforms.Android.Abstract;
 using Soenneker.Maui.Admob.Platforms.Android.Extensions;
+using System;
+using System.Collections.Generic;
 
 namespace Soenneker.Maui.Admob.Platforms.Android.Banner;
 
@@ -16,63 +18,66 @@ public class BannerAdHandler : ViewHandler<BannerAd, AdView>
 {
     public static PropertyMapper<BannerAd, BannerAdHandler> PropertyMapper = new(ViewMapper);
 
-    private readonly List<string>? _testDevices;
-    private readonly string _unitId;
+    private List<string>? _testDevices;
+    private string _unitId = AdmobUnitIdConstants.Banner;
+    private EventHandler<AdError>? _onAdFailedToLoad;
 
-    public BannerAdHandler(IConfiguration config, IAdMobServiceUtil adMobServiceUtil) : base(PropertyMapper)
+    // Requires empty parameters constructor
+    public BannerAdHandler() : base(PropertyMapper)
     {
-        _testDevices = config.GetValue<List<string>?>("AdMob:TestDevices");
-        var unitId = config.GetValue<string?>("AdMob:BannerUnitId");
-        var testMode = config.GetValue<bool>("AdMob:TestMode");
+    }
 
-        if (testMode || unitId.IsNullOrEmpty())
+    protected override void ConnectHandler(AdView platformView)
+    {
+        base.ConnectHandler(platformView);
+
+        if (MauiContext?.Services is IServiceProvider services)
         {
-            _unitId = AdmobUnitIdConstants.Banner;
+            var config = services.GetService<IConfiguration>();
+            var adMobServiceUtil = services.GetService<IAdMobServiceUtil>();
+
+            if (config != null)
+            {
+                _testDevices = config.GetValue<List<string>?>("AdMob:TestDevices");
+                var unitId = config.GetValue<string?>("AdMob:BannerUnitId");
+                var testMode = config.GetValue<bool>("AdMob:TestMode");
+
+                _unitId = testMode || string.IsNullOrEmpty(unitId)
+                    ? AdmobUnitIdConstants.Banner
+                    : unitId;
+            }
+
+            adMobServiceUtil?.Init();
         }
 
-        adMobServiceUtil.Init();
-    }
+        platformView.AdUnitId = _unitId;
 
-    protected override void DisconnectHandler(AdView platformView)
-    {
-        platformView.Dispose();
-        base.DisconnectHandler(platformView);
-    }
+        var configBuilder = new RequestConfiguration.Builder();
+        if (_testDevices.Populated())
+            configBuilder.SetTestDeviceIds(_testDevices);
 
-    protected override AdView CreatePlatformView()
-    {
-        AdView adView = BuildView();
+        MobileAds.RequestConfiguration = configBuilder.Build();
+        platformView.LoadAd(new AdRequest.Builder().Build());
 
-        adView.AdListener = BuildListener();
-        
-        Load(adView);
-
-        return adView;
-    }
-
-    private AdView BuildView()
-    {
-        AdSize adSize;
-
-        if (VirtualView.Size == AdmobAdSize.Custom)
-            adSize = new AdSize(VirtualView.ContentWidth, VirtualView.ContentHeight);
-        else
-            adSize = VirtualView.Size.ToAdSize();
-
-        var adView = new AdView(Context)
+        if (VirtualView != null)
         {
-            AdSize = adSize,
-            AdUnitId = _unitId
-        };
-
-        return adView;
+            VirtualView.HeightRequest = platformView.AdSize!.Height;
+            VirtualView.WidthRequest = platformView.AdSize!.Width;
+        }
     }
 
     private BannerAdListener BuildListener()
     {
         var listener = new BannerAdListener();
         listener.AdLoaded += VirtualView.OnLoadedFired;
-        listener.AdFailedToLoad += (s, e) => VirtualView.OnFailedToLoadFired(s, new ProblemDetailsDto { Title = e.Message });
+
+        _onAdFailedToLoad = (s, e) =>
+        {
+            var adError = new AdError(e.Code, e.Message, e.Domain);
+            VirtualView?.OnFailedToLoadFired(s, new ProblemDetailsDto { Title = adError.Message });
+        };
+
+        listener.AdFailedToLoad += _onAdFailedToLoad;
         listener.AdImpression += VirtualView.OnImpressionFired;
         listener.AdClicked += VirtualView.OnClickedFired;
         listener.AdOpened += VirtualView.OnOpenedFired;
@@ -82,21 +87,47 @@ public class BannerAdHandler : ViewHandler<BannerAd, AdView>
         return listener;
     }
 
-    private void Load(AdView adView)
+    protected override void DisconnectHandler(AdView platformView)
     {
-        var configBuilder = new RequestConfiguration.Builder();
+        if (platformView.AdListener is BannerAdListener listener)
+        {
+            listener.AdLoaded -= VirtualView.OnLoadedFired;
 
-        if (_testDevices.Populated())
-            configBuilder.SetTestDeviceIds(_testDevices);
+            if (_onAdFailedToLoad != null)
+            {
+                listener.AdFailedToLoad -= _onAdFailedToLoad;
+                _onAdFailedToLoad = null;
+            }
 
-        MobileAds.RequestConfiguration = configBuilder.Build();
+            listener.AdImpression -= VirtualView.OnImpressionFired;
+            listener.AdClicked -= VirtualView.OnClickedFired;
+            listener.AdOpened -= VirtualView.OnOpenedFired;
+            listener.AdClosed -= VirtualView.OnClosedFired;
+            listener.AdSwiped -= VirtualView.OnSwipedFired;
+        }
 
-        var requestBuilder = new AdRequest.Builder();
-        AdRequest adRequest = requestBuilder.Build();
+        platformView.Dispose();
+        base.DisconnectHandler(platformView);
+    }
 
-        adView.LoadAd(adRequest);
+    protected override AdView CreatePlatformView()
+    {
+        AdView adView = BuildView();
+        adView.AdListener = BuildListener();
 
-        VirtualView.HeightRequest = adView.AdSize!.Height;
-        VirtualView.WidthRequest = adView.AdSize!.Width;
+        return adView;
+    }
+
+    private AdView BuildView()
+    {
+        AdSize adSize = VirtualView?.Size == AdmobAdSize.Custom
+            ? new AdSize(VirtualView.ContentWidth, VirtualView.ContentHeight)
+            : VirtualView?.Size.ToAdSize() ?? AdSize.Banner;
+
+        return new AdView(Context)
+        {
+            AdSize = adSize,
+            AdUnitId = _unitId
+        };
     }
 }
